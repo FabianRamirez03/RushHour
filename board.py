8e6cf99db89e4c1af5ee59c03ef946f9274f5748
Fabian Ramirez <ramirezfabian003@gmail.com>
2022-03-30 00:26:37 -0600

    Reconocimiento de casillas listo

--- board.py
+++ board.py
@@ -1,24 +1,39 @@
 import pygame
 
 TILESIZE = 44
+BOARD_POS = (58, 185)
 
-def create_board_surf():
-    board_surf = pygame.Surface((TILESIZE*9, TILESIZE*9))
-    board_surf.set_colorkey((0, 0, 0))  #transparent mode
+
+def get_square_under_mouse(board):
+    mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
+    x = get_X_Under_Mouse(int(mouse_pos[0]))
+    y = get_Y_Under_Mouse(int(mouse_pos[1]))
+    try:
+        if x != None and y != None:
+            if x >= 0 and y >= 0: return board[y][x], x, y
+    except IndexError:
+        pass
+    return None, None, None
+
+
+def create_board_surf() -> object:
+    board_surf = pygame.Surface((TILESIZE * 9, TILESIZE * 9))
+    board_surf.set_colorkey((0, 0, 0))  # transparent mode
     SPACE = 13
     xSpace = SPACE
     ySpace = SPACE
     dark = False
     for y in range(6):
         for x in range(6):
-            rect = pygame.Rect((x*TILESIZE)+xSpace, (y*TILESIZE)+ySpace, TILESIZE, TILESIZE)
-            pygame.draw.rect(board_surf, pygame.Color('indianred1' if dark else 'indianred1'), rect)
+            rect = pygame.Rect((x * TILESIZE) + xSpace, (y * TILESIZE) + ySpace, TILESIZE, TILESIZE)
+            pygame.draw.rect(board_surf, pygame.Color('indianred1' if dark else 'red'), rect)
             dark = not dark
             xSpace = xSpace + SPACE
         dark = not dark
         ySpace = ySpace + SPACE
         xSpace = SPACE
     return board_surf
+
 
 def create_board():
     board = []
@@ -28,3 +43,36 @@ def create_board():
             board[y].append(None)
 
     return board
+
+
+def get_X_Under_Mouse(x):
+    if 56 <= x <= 100:
+        return 0
+    if 113 <= x <= 157:
+        return 1
+    if 160 <= x <= 214:
+        return 2
+    if 227 <= x <= 271:
+        return 3
+    if 284 <= x <= 328:
+        return 4
+    if 341 <= x <= 385:
+        return 5
+    else:
+        return None
+
+def get_Y_Under_Mouse(y):
+    if 185 <= y <= 229:
+        return 0
+    if 242 <= y <= 286:
+        return 1
+    if 299 <= y <= 343:
+        return 2
+    if 356 <= y <= 400:
+        return 3
+    if 413 <= y <= 457:
+        return 4
+    if 470 <= y <= 514:
+        return 5
+    else:
+        return None
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-#Rush Hour Game
+# Rush Hour Game
 
 # Imports
 import pygame
 import button
 import board
 
-#Constants
+# Constants
 SCREEN_WIDTH = 700
 SCREEN_HEIGHT = 600
-
+BOARD_POS = (58, 185)
+TILESIZE = 44
+SPACE = 13
 
 pygame.init()
 
@@ -16,8 +18,7 @@ pygame.init()
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 pygame.display.set_caption("Rush Hour Game")
 
-
-#images
+# images
 image = pygame.image.load(r'./images/Home.PNG')
 start_img = pygame.image.load(r'./images/start_btn.png').convert_alpha()
 exit_img = pygame.image.load(r'./images/exit_btn.png').convert_alpha()
@@ -25,23 +26,26 @@ play_img = pygame.image.load(r'./images/play_btn.png').convert_alpha()
 board_img = pygame.image.load(r'./images/board.png').convert_alpha()
 header_img = pygame.image.load(r'./images/header.png').convert_alpha()
 
-#create button instances
-start_btn = button.Button(250,350, start_img,1)
-play_btn = button.Button(480,515,play_img,1)
-exit_btn  = button.Button(250,450,exit_img,1)
+# create button instances
+start_btn = button.Button(250, 350, start_img, 1)
+play_btn = button.Button(480, 515, play_img, 1)
+exit_btn = button.Button(250, 450, exit_img, 1)
 
+font = pygame.font.SysFont("Times New Roman", 30)
 
-font = pygame.font.SysFont("Times New Roman",30)
-
-#!!!!!!!!!!!!!!!!!!!
-#Scenes details
-#sceneIndex = 0 -> menu
-#sceneIndex = 1 -> initial config
-#sceneIndex = 2 -> game
+# !!!!!!!!!!!!!!!!!!!
+# Scenes details
+# sceneIndex = 0 -> menu
+# sceneIndex = 1 -> initial config
+# sceneIndex = 2 -> game
 
 sceneIndex = 0
+game_board = board.create_board()
+board_surf = board.create_board_surf()
+# Graphic Constants
 
-#Menu Scene
+
+# Menu Scene
 def drawMenuScene(screen):
     global sceneIndex, running
     screen.fill((255, 255, 255))
@@ -49,34 +53,35 @@ def drawMenuScene(screen):
     screen.blit(image, (15, 100))
     # buttons (Start and Exit)
     if start_btn.draw(screen) == True:
-        sceneIndex = 1   #going to initial config scene
+        sceneIndex = 1  # going to initial config scene
 
     if exit_btn.draw(screen) == True:
-        running = False  #quit
+        running = False  # quit
 
-#Inital Config Scene
+
+# Inital Config Scene
 def drawConfigScene(screen):
     global running, sceneIndex
-    game_board = board.create_board()
-    board_surf = board.create_board_surf()
     screen.fill((255, 255, 255))
     screen.blit(board_img, (10, 120))
     screen.blit(header_img, (180, 10))
     screen.blit(board_surf, (44, 172))
 
     if play_btn.draw(screen) == True:
-        sceneIndex = 2 #going to game scene
+        sceneIndex = 2  # going to game scene
 
-#Game scene
+
+# Game scene
 def drawGameScene(screen):
     screen.fill((0, 0, 0))
     text = font.render("Game", True, (255, 255, 255))
     screen.blit(text, (300, 220, 500, 200))
 
-#Select which scene draw acording to scene index
+
+# Select which scene draw acording to scene index
 def drawScene(screen):
     global sceneIndex
-    #start
+    # start
     if sceneIndex == 0:
         drawMenuScene(screen)
 
@@ -90,22 +95,25 @@ def drawScene(screen):
 # Run until the user asks to quit
 running = True
 while running:
-
+    clock = pygame.time.Clock()
     # Did the user click the window close button?
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN and sceneIndex != 0:
             if event.key == pygame.K_ESCAPE:
-                sceneIndex = 0 #going back to menu
+                sceneIndex = 0  # going back to menu
 
     # Fill the background with white
     screen.fill((255, 255, 255))
     drawScene(screen)
     # Flip the display
+    if sceneIndex == 1:
+        piece, x, y = board.get_square_under_mouse(game_board)
+        if x != None:
+            rect = (BOARD_POS[0] + (x * TILESIZE) + (x*SPACE), BOARD_POS[1] + (y * TILESIZE) + (y*SPACE) , TILESIZE, TILESIZE)
+            pygame.draw.rect(screen, (0, 0, 255, 50), rect, 2)
     pygame.display.flip()
-
+    clock.tick(30)
 # Done! Time to quit.
 pygame.quit()
-
-
