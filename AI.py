491673de8eec037efad076a7c920348e126bb6c4
Sebastian <scyock.03@gmail.com>
2022-04-22 01:25:54 -0600

    Incorporated better heuristic

--- AI.py
+++ AI.py
@@ -4,50 +4,13 @@ N = 6
 EMPTY_SPACE = '_'
 ICE_CREAM_TRUCK = 'A'
 START_ROW = 2
-
-
-def get_board():
-  # Uppercase is horizontal, lowercase is vertical.
-  board = [[EMPTY_SPACE] * 6 for _ in range(N)]
-  # Initialize the ice cream truck in a random column.
-  start_col = random.randrange(N - 2)
-  board[START_ROW][start_col] = board[START_ROW][start_col + 1] = ICE_CREAM_TRUCK
-
-  # Add more cars.
-  num_attempts = 0
-  for i in range(random.randrange(6, 10)):
-    car_len = random.randrange(2, 4)
-    while True:
-      vertical = random.randrange(2) == 0
-      r = random.randrange(N - (car_len - 1) * int(vertical))
-      c = random.randrange(N - (car_len - 1) * int(not vertical))
-      is_clear = True
-      for j in range(car_len):
-        if board[r + j * int(vertical)][c + j * int(not vertical)] != EMPTY_SPACE:
-          is_clear = False
-          break
-
-      if is_clear:
-        car_char = chr(ord('b' if vertical else 'B') + i)
-        for j in range(car_len):
-          board[r + j * int(vertical)][c + j * int(not vertical)] = car_char
-        break
-
-      num_attempts += 1
-      if num_attempts > 1000:
-        # We have enough cars anyway.
-        break
-
-  return board
-
+EXIT_ROW = 2
 
 def board_str(board):
   return '\n'.join(''.join(_) for _ in board)
 
-
 def copy_board(board):
   return [_[:] for _ in board]
-
 
 def is_solved(board):
   # Find any obstacles between the ice cream truck and the right edge.
@@ -61,7 +24,6 @@ def is_solved(board):
       return False
 
   return True
-
 
 def get_next_states(board):
   processed_chars_set = set([EMPTY_SPACE])
@@ -104,7 +66,6 @@ def get_next_states(board):
 
   return next_states
 
-
 PLIES = {}
 def search(board):
   queue = [(0, [board])]
@@ -120,24 +81,93 @@ def search(board):
     if is_solved(path[-1]):
       return path
 
-    for next_state in get_next_states(path[-1]):
+    sorted_states = sort_heuristics(get_next_states(path[-1]))
+    for next_state in sorted_states:
       if board_str(next_state) not in board_hash_set:
         board_hash_set.add(board_str(next_state))
         queue.append((ply + 1, path + [next_state]))
-
   return []
 
-
 def solve(board):
+  calc_heuristic(board)
   path = search(board)
-  #print('Solved length: {}'.format(len(path)))
-  #print("Piles", PLIES)
-
-  if len(path) >= 1:
-    #print('\n\n'.join(board_str(_) for _ in path))
-    #print("solution", path)
-    return path
-  else:
-    return []
+  return path
 
 
+# ***************************
+#   HEURISTIC CALCULATION
+# ***************************
+def sort_heuristics(possible_next_states):
+  sorted_states = []
+  heuristics_states = []
+  for state in possible_next_states:
+    heuristics_states.append([calc_heuristic(state), state])
+  heuristics_states.sort(key=lambda x: x[0])
+  for state in heuristics_states:
+    sorted_states.append(state[1])
+  return sorted_states
+
+def calc_heuristic(board):
+  result = 0
+  blockers = get_blocking_cars(board[EXIT_ROW])
+  min_sub_blockers = get_min_subblocking_cars(board, blockers)
+  for sub in min_sub_blockers:
+    result += len(sub)
+  result += len(blockers)
+  return result
+
+def get_blocking_cars(exit_row):
+  result = []
+  unique_cars = remove_repeated(exit_row)
+  red_car_pos = unique_cars.index('A')
+  for i in range(red_car_pos+1, len(unique_cars)):
+    if unique_cars[i] != "_":
+      result.append(unique_cars[i])
+  return result
+
+def get_board_column(board, column):
+  result = []
+  for line in board:
+    result.append(line[column])
+  return result
+
+def get_blocking_columns(board, blocking_cars):
+  blocking_cars_positions = []
+  result = []
+  for car in blocking_cars:
+    blocking_cars_positions.append(board[EXIT_ROW].index(car))
+  for pos in blocking_cars_positions:
+    result.append(remove_repeated(get_board_column(board, pos)))
+  return result
+
+def get_min_subblocking_cars(board, blocking_cars):
+  result = []
+  blocking_columns = get_blocking_columns(board, blocking_cars)
+  for i in range(len(blocking_columns)):
+    top, bottom = get_top_bot_blockers(blocking_columns[i], blocking_cars[i])
+    result.append(remove_blanks(top)) if len(top)<len(bottom) else result.append(remove_blanks(bottom))
+  return result
+
+def get_top_bot_blockers(column, separator):
+  index = column.index(separator)
+  top = column[:index]
+  column.reverse()
+  index = column.index(separator)
+  bottom = column[:index]
+  bottom.reverse()
+  return top, bottom
+
+def remove_blanks(row):
+  result = []
+  for elem in row:
+    if elem!= '_': result.append(elem)
+  return result
+
+def remove_repeated(row):
+  result = []
+  found = []
+  for elem in row:
+    if elem not in found:
+      result.append(elem)
+      found.append(elem)
+  return result
